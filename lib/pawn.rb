c6af5d8512904dd7eaf2f8d0aa4051b71ac9fd9a
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-04-09 17:50:37 +0800

    refactor: Separate big functions to smaller ones

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -12,44 +12,61 @@ class Pawn < Piece
   end
 
   def valid_moves
-    moves = []
     current_index = get_name_index(@current_position)
 
     # white goes up vertically in the 2d array and black goes the opposite way
-    # forward move
+    # forward movement along y_index
     direction = @color == :white ? -1 : 1
-    forward = square_name(current_index[1], current_index[0] + direction)
-    moves.push(forward) if @board.empty?(forward)
 
+    forward = get_forward_move(current_index[1], current_index[0], direction)
     diagonal_moves = get_diagonal_moves(current_index[1], current_index[0], direction)
+    double = get_double_move(current_index[1], current_index[0], direction)
+
+    forward + diagonal_moves + double
+  end
+
+  def get_forward_move(x_index, y_index, direction)
+    forward = square_name(x_index, y_index + direction)
+
+    @board.empty?(forward) ? [forward] : []
+  end
+
+  def get_diagonal_moves(x_index, y_index, direction)
+    diagonal_moves = []
+    diagonal_moves.push(square_name(x_index + 1, y_index + direction)) if x_index + 1 < BOARD_SIZE
+    diagonal_moves.push(square_name(x_index - 1, y_index + direction)) unless (x_index - 1).negative?
+
+    valid_diagonal_moves = []
 
     diagonal_moves.each do |move|
       next if @board.empty?(move)
 
-      moves << move if opponent?(move)
+      valid_diagonal_moves << move if opponent?(move)
     end
 
-    double = get_double_move(current_index[1], current_index[0], direction)
-    moves.push(double) if !double.nil? && @board.empty?(double) && @board.empty?(forward)
-
-    moves
-  end
-
-  def get_diagonal_moves(x_index, y_index, direction)
-    moves = []
-    moves.push(square_name(x_index + 1, y_index + direction)) if x_index + 1 < BOARD_SIZE
-    moves.push(square_name(x_index - 1, y_index + direction)) unless (x_index - 1).negative?
-
-    moves
+    valid_diagonal_moves
   end
 
   def get_double_move(x_index, y_index, direction)
+    forward = square_name(x_index, y_index + direction)
     first_position = @color == :white ? 6 : 1
 
-    if @color == :white
-      first_position == y_index ? square_name(x_index, y_index + (direction * 2)) : nil
-    else
-      first_position == y_index ? square_name(x_index, y_index + (direction * 2)) : nil
+    double = double_move(x_index, y_index, direction, first_position)
+
+    valid_double?(double, forward) ? [double] : []
+  end
+
+  def valid_double?(double, forward)
+    !double.nil? && @board.empty?(double) && @board.empty?(forward)
+  end
+
+  def double_move(x_index, y_index, direction, first_position)
+    if first_position == y_index
+      return square_name(x_index, y_index + (direction * 2)) if @color == :white
+
+      return square_name(x_index, y_index + (direction * 2))
     end
+
+    nil
   end
 end
