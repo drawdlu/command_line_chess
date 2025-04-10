8888163ef3909561c5734425c442afcd60f0766d
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-04-10 19:45:51 +0800

    feat: Check if bishop move is valid

---
+++ lib/bishop.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require_relative 'piece'
+
+# Controls bishop movement
+class Bishop < Piece
+  def valid_move?(position)
+    x_direction = get_direction(@current_position[0], position[0])
+    y_direction = get_direction(@current_position[1], position[1])
+
+    @current_position[0] != position[0] &&
+      @current_position[1] != position[1] &&
+      same_distance?(position) &&
+      no_pieces_in_direction?(position, x_direction, y_direction) &&
+      (@board.empty?(position) ||
+      opponent?(position))
+  end
+
+  def same_distance?(position)
+    x_distance = (@current_position[0].ord - position[0].ord).abs
+    y_distance = (@current_position[1].ord - position[1].ord).abs
+
+    x_distance == y_distance
+  end
+end
