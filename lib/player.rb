9397175e4c102e10efe658f3f7bfef4945f77140
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-04-11 19:03:48 +0800

    feat: Create class to handle player name and color
    Gets player name at initialization

---
+++ lib/player.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# Handles player name and color
+class Player
+  def initialize(color)
+    @name = get_name(color)
+    @color = color
+  end
+
+  def get_name(color)
+    name = ''
+
+    while name == ''
+      print "Enter player name for #{color.upcase} pieces: "
+      name = gets.chomp
+    end
+
+    name
+  end
+end
