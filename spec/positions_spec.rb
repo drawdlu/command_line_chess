85fbb52dbe395eaaa3d4ef4c120f1998bccdd57b
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-04-11 23:03:39 +0800

    test: Change test expected values to hash

--- spec/positions_spec.rb
+++ spec/positions_spec.rb
@@ -4,16 +4,16 @@ describe Positions do
   let(:position_mod) { Class.new { extend Positions } }
   describe '#get_name_index' do
     context 'when "A1" is passed in' do
-      it 'will return [7, 0]' do
+      it 'will return { x: 7, y: 0 }' do
         result = position_mod.get_name_index('A1')
-        expect(result).to eq([7, 0])
+        expect(result).to eq({ x: 7, y: 0 })
       end
     end
 
     context 'when "D5" is passed in' do
-      it 'will return [3, 3]' do
+      it 'will return { x: 3, y: 3 }' do
         result = position_mod.get_name_index('D5')
-        expect(result).to eq([3, 3])
+        expect(result).to eq({ x: 3, y: 3 })
       end
     end
   end
