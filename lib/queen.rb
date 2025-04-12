d2bc5aed671c53f4262aab9d1fb901e66e840dfe
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-04-13 03:15:35 +0800

    feat: Get all valid moves of Queen

--- lib/queen.rb
+++ lib/queen.rb
@@ -10,4 +10,8 @@ class Queen < Piece
   def to_s
     color == :white ? '♛ ' : '♕ '
   end
+
+  def all_valid_moves
+    directional_moves(DIAGONAL) + directional_moves(HORIZONTAL_VERTICAL)
+  end
 end
