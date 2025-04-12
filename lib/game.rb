e9a3fc6e42e46f02fc4a829092866e18035eb140
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-04-12 21:39:10 +0800

    feat: Get player move

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,43 @@
 # frozen_string_literal: true
 
 require_relative '../lib/board'
+require_relative '../lib/positions'
 
 # Controls game loop and special conditions
 class Game
+  include Positions
+
   def initialize
     @board = Board.new
   end
 
   private
+
+  def get_start_position
+    move = ''
+    while move == ''
+      print 'Pick a piece to move: '
+      move = gets.chomp
+
+      return move.upcase if valid_position?(move) &&
+                            !@board.empty?(move) &&
+                            ally?(move)
+
+      move = ''
+    end
+  end
+
+  def get_move_position(piece)
+    move = ''
+    while move == ''
+      print 'Pick a square to move to: '
+      move = gets.chomp
+
+      return move.upcase if valid_position?(move) && piece.valid_move?(move)
+
+      move = ''
+    end
+  end
 
   def valid_position?(position)
     valid_positions = /[A-Ha-h][1-8]/
@@ -29,3 +58,7 @@ class Game
     end
   end
 end
+
+test = Game.new
+
+test.get_start_position
