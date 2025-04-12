c2b1885eb9952d98289376ab2f8eb9f60e3936f3
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-04-12 21:01:54 +0800

    feat: Check for ally and remove piece on board list

--- lib/game.rb
+++ lib/game.rb
@@ -12,4 +12,18 @@ class Game
     valid_positions = /[A-Ha-h][1-8]/
     valid_positions.match?(position)
   end
+
+  def ally?(position)
+    index = get_name_index(position)
+
+    @board.board[index[:x]][index[:y]].color == @active_player.color
+  end
+
+  def remove_piece(piece)
+    if piece.color == :white
+      @board.white_pieces.delete(piece)
+    else
+      @board.black_pieces.delete(piece)
+    end
+  end
 end
