e0c8a3bd49c8ab71234857dadca552daf5896dcf
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-04-13 05:17:42 +0800

    feat: Get all King valid moves

--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -48,4 +48,35 @@ describe King do
       end
     end
   end
+
+  describe '#all_valid_moves' do
+    subject(:king) { described_class.new(:white, 'E1', Board.new) }
+    context 'when it is at start and no pieces has moved' do
+      it 'will return Set[]' do
+        result = king.all_valid_moves
+        expect(result).to be_empty
+      end
+    end
+
+    context 'when it is at start and E2 is empty' do
+      it 'will return Set[E2]' do
+        new_board = king.instance_variable_get(:@board)
+        new_board.board[6][4] = nil
+        king.instance_variable_set(:@board, new_board)
+        result = king.all_valid_moves
+        expect(result).to eq(Set['E2'])
+      end
+    end
+
+    context 'when it is at start and E2 is empty' do
+      subject(:king) { described_class.new(:white, 'D4', Board.new) }
+      it 'will return Set[E3, C3, D3, E5, C5, D5, E4, C4]' do
+        new_board = king.instance_variable_get(:@board)
+        new_board.board[4][3] = king
+        king.instance_variable_set(:@board, new_board)
+        result = king.all_valid_moves
+        expect(result).to eq(Set['E3', 'C3', 'D3', 'E5', 'C5', 'D5', 'E4', 'C4'])
+      end
+    end
+  end
 end
