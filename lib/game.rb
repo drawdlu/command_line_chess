67e04c6b8ce73c79493ce45ec6dd5675442e1e9e
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-04-13 20:18:11 +0800

    refactor: Separate into multiple methods
    Easier for testing

--- lib/game.rb
+++ lib/game.rb
@@ -18,17 +18,31 @@ class Game
   def start
     loop do
       puts @board
-      prompt_player
-      initial_pos = get_start_position
-      piece = get_piece(initial_pos)
-      final_pos = get_move_position(piece)
-      @board.move_to(initial_pos, final_pos)
-      piece.update_position(final_pos)
-      @active_player = @active_player == @white ? @black : @white
+      move = ask_for_move
+      apply_move(move[:initial_pos], move[:final_pos])
+      update_active_player
     end
   end
 
   private
+
+  def update_active_player
+    @active_player = @active_player == @white ? @black : @white
+  end
+
+  def ask_for_move
+    initial_pos = ask_start_position
+    piece = get_piece(initial_pos)
+    final_pos = get_move_position(piece)
+
+    { final_pos: final_pos, initial_pos: initial_pos }
+  end
+
+  def apply_move(initial_pos, final_pos)
+    piece = get_piece(initial_pos)
+    @board.move_to(initial_pos, final_pos)
+    piece.update_position(final_pos)
+  end
 
   def prompt_player
     puts "#{@active_player.name}'s turn to move"
@@ -41,7 +55,7 @@ class Game
     @board.board[index[:x]][index[:y]]
   end
 
-  def get_start_position
+  def ask_start_position
     move = ''
     while move == ''
       print 'PIECE: '
@@ -56,10 +70,12 @@ class Game
   end
 
   def valid_start?(position)
+    piece = get_piece(position)
+
     valid_position?(position) &&
       !@board.empty?(position) &&
       ally?(position) &&
-      !get_piece(position).all_valid_moves.empty?
+      !piece.all_valid_moves.empty?
   end
 
   def get_move_position(piece)
