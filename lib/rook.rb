d4d9a797e927e6089a376bc04dc4ad58c93eb32a
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-04-17 23:20:06 +0800

    feat: Add valid castling moves for rook
    Not yet implemented on board

--- lib/rook.rb
+++ lib/rook.rb
@@ -16,11 +16,23 @@ class Rook < Piece
   end
 
   def all_valid_moves
-    directional_moves(HORIZONTAL_VERTICAL)
+    directional_moves(HORIZONTAL_VERTICAL) + castling
   end
 
   def update_position(position)
     @current_position = position
     @moved = true
+  end
+
+  private
+
+  def castling
+    moves = Set[]
+    king_position = @color == :white ? 'E1' : 'E8'
+
+    moves.add(king_position) if ally_of_class_has_not_moved?(king_position, King) &&
+                                valid_castling_move?(king_position)
+
+    moves
   end
 end
--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -41,4 +41,54 @@ describe Rook do
       end
     end
   end
+
+  describe '#castling' do
+    let(:pieces) do
+      [{ color: :white, position: 'E1', class: King },
+       { color: :white, position: 'H1', class: Rook },
+       { color: :white, position: 'A1', class: Rook }]
+    end
+    let(:board) { create_dummy(pieces) }
+
+    context 'when rook and king has not moved, no opponents control space between' do
+      it 'will return position of King E1' do
+        right_rook = board.board[7][7]
+        left_rook = board.board[7][0]
+        right_result = right_rook.send(:castling)
+        left_result = left_rook.send(:castling)
+        king_position = Set['E1']
+        expect(right_result).to eq(king_position)
+        expect(left_result).to eq(king_position)
+      end
+    end
+
+    context 'when only left rook has moved and king has not moved, no opponents control space between' do
+      it 'will return position of King E1 for right only' do
+        right_rook = board.board[7][7]
+        left_rook = board.board[7][0]
+        left_rook.instance_variable_set(:@moved, true)
+        right_result = right_rook.send(:castling)
+        left_result = left_rook.send(:castling)
+        king_position = Set['E1']
+        empty_set = Set[]
+        expect(right_result).to eq(king_position)
+        expect(left_result).to eq(empty_set)
+      end
+    end
+
+    context 'king and rook not moved and an enemy is controlling left rook squares' do
+      it 'will return King position for right rook only' do
+        black_rook = Rook.new(:black, 'B7', board)
+        insert_piece(black_rook)
+        right_rook = board.board[7][7]
+        left_rook = board.board[7][0]
+        right_result = right_rook.send(:castling)
+        left_result = left_rook.send(:castling)
+        king_position = Set['E1']
+        empty_set = Set[]
+        expect(right_result).to eq(king_position)
+        expect(left_result).to eq(empty_set)
+      end
+    end
+  end
 end
